d27cb17ebdb92a53426abc9f703f078f66dc0475
Matheus Harsani <Matheus.Harsani@keyruscorp.key>
2018-09-30 23:16:57 -0300

    add some styles

--- my-app/src/components/search/index.js
+++ my-app/src/components/search/index.js
@@ -1,14 +1,19 @@
 import React from 'react'
-import { Dropdown } from 'semantic-ui-react'
+import { Dropdown, Grid } from 'semantic-ui-react'
+
 
 const Search = ({handleFilter, dataSelect, handleSelect, isDisabled}) => (
-<div className="grid">
-  <Dropdown placeholder='Select your option' selection  options={dataSelect} onChange={handleSelect} />
-    <div className='ui big icon input'>
-        <input type='text' placeholder='Search...' onChange={handleFilter} disabled={isDisabled}  />
-        <i aria-hidden='true' className='search icon' />
-    </div>
-</div>
+ <Grid columns='equal'>
+    <Grid.Column  width={4}>
+        <Dropdown placeholder='Select your option' selection  options={dataSelect} onChange={handleSelect} />
+    </Grid.Column>
+    <Grid.Column>
+        <div className='ui icon input'>
+            <input type='text' placeholder='Search...' onChange={handleFilter} disabled={isDisabled}  />
+            <i aria-hidden='true' className='search icon' />
+        </div>
+    </Grid.Column>
+  </Grid>
 )
 
 export default Search
--- my-app/src/container/app-container.js
+++ my-app/src/container/app-container.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Table from '../components/table/index'
 import Search from '../components/search/index'
+import { Header } from 'semantic-ui-react'
 
 const AppContainer = (
     {errorInfo, pagination, query, selectVal, isDisabled,  dataSelect, handlePagination, nextPagination, previousPagination, handleFilter, handleSelect}) => (
     <div className="ui container">
         <div className="App-intro">
-          <Search handleFilter={handleFilter} dataSelect={dataSelect} handleSelect={handleSelect} isDisabled={isDisabled}/>
+          <Header as='h3' block>
+            <Search handleFilter={handleFilter} dataSelect={dataSelect} handleSelect={handleSelect} isDisabled={isDisabled}/>
+          </Header>
           <Table errorInfo={query ? errorInfo.filter(x => x[selectVal].toLowerCase().includes(query.toLowerCase()) !== false) : errorInfo} 
           pagination={pagination} 
           handlePagination={(clicked) => handlePagination(clicked)} 
